c967542c91822382fa47012e44c6417901feffe0
PavelVorobyev77 <pascha27_05@mail.ru>
2024-03-08 19:31:16 +0700

    5

--- app/src/main/java/com/example/dictophone_vorobyevp/MainActivity.kt
+++ app/src/main/java/com/example/dictophone_vorobyevp/MainActivity.kt
@@ -6,14 +6,21 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
+import android.os.Handler
+import android.os.Looper
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -31,6 +38,11 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
     private lateinit var tvTimer: TextView
     private lateinit var recorder: MediaRecorder
     private lateinit var waveformView: WaveformView
+    private lateinit var bottomSheet: LinearLayout
+    private lateinit var bottomSheetBG: View
+    private lateinit var filenameInput: TextInputEditText
+    private lateinit var btnCancel: MaterialButton
+    private lateinit var btnOk: MaterialButton
     private var dirPath = ""
     private var filename = ""
     private var isRecording = false
@@ -40,18 +52,25 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
 
     private lateinit var timer: Timer
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnRecord = findViewById(R.id.btnRecord)
-        btnList= findViewById(R.id.btnList)
-        btnDone= findViewById(R.id.btnDone)
-        btnDelete= findViewById(R.id.btnDelete)
-
+        btnList = findViewById(R.id.btnList)
+        btnDone = findViewById(R.id.btnDone)
+        btnDelete = findViewById(R.id.btnDelete)
+        btnCancel = findViewById(R.id.btnCancel)
+        btnOk = findViewById(R.id.btnOk)
         tvTimer = findViewById(R.id.tvTimer)
 
         waveformView = findViewById(R.id.waveformView)
+        bottomSheet = findViewById(R.id.bottomSheet)
+        bottomSheetBG = findViewById(R.id.bottomSheetBG)
+        filenameInput = findViewById(R.id.filenameInput)
 
         permissionGranted = ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED
 
@@ -59,8 +78,13 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
             ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
         }
 
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.peekHeight = 0
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+
         timer = Timer(this)
-        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
 
         btnRecord.setOnClickListener {
             when {
@@ -78,17 +102,57 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
 
         btnDone.setOnClickListener{
             stopRecorder()
-            //...
+
             Toast.makeText(this, "Запись сохранена", Toast.LENGTH_SHORT).show()
+
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetBG.visibility = View.VISIBLE
+            filenameInput.setText(filename)
+        }
+
+        btnOk.setOnClickListener {
+            dismiss()
+            save()
+        }
+
+        btnCancel.setOnClickListener {
+            File("$dirPath$filename.mp3").delete()
+            dismiss()
+
+        }
+
+        bottomSheetBG.setOnClickListener{
+            File("$dirPath$filename.mp3").delete()
+            dismiss()
         }
 
         btnDelete.setOnClickListener{
             stopRecorder()
-            File("$dirPath$filename.mp3")
+            File("$dirPath$filename.mp3").delete()
             Toast.makeText(this, "Запись удалена", Toast.LENGTH_SHORT).show()
         }
 
         btnDelete.isClickable = false
+    }
+
+    private fun save(){
+        val newFilename = filenameInput.text.toString()
+        if(newFilename != filename){
+            var newFile = File("$dirPath$newFilename.mp3")
+            File("$dirPath$filename.mp3").renameTo(newFile)
+        }
+    }
+    private fun dismiss(){
+        bottomSheetBG.visibility = View.GONE
+        hideKeyboard(filenameInput)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }, 100)
+    }
+    private fun hideKeyboard(view: View){
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     override fun onRequestPermissionsResult(
